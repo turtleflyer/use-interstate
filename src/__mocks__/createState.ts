ce563797a0cd4056a54e44c46d2f7fc4a7b738e5
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2021-03-29 23:51:22 -0700

    Optimize implementation of mock

--- src/__mocks__/createState.ts
+++ src/__mocks__/createState.ts
@@ -1,9 +1,8 @@
 import type * as CreateStateImport from '../createState';
 import { traverseLinkedList } from '../LinkedList';
-import type { State, StateEntry } from '../State';
+import type { StateEntry } from '../State';
 import type { InterstateKey } from '../UseInterstateTypes';
 
-let memoizeMapOnceStateCreated: Map<InterstateKey, StateEntry<unknown>>;
 let originalMap: Map<InterstateKey, StateEntry<unknown>>;
 
 global.Map = class MockedMap extends Map {
@@ -13,23 +12,10 @@ global.Map = class MockedMap extends Map {
   }
 };
 
-const { createState: originalCreateState }: typeof CreateStateImport = jest.requireActual(
-  '../createState.ts'
-);
-
-const mockedCreateState = jest.fn(
-  (): State => {
-    const originalState = originalCreateState();
-    memoizeMapOnceStateCreated = originalMap;
-
-    return originalState;
-  }
-);
-
 export type TriggersCounter = (key: InterstateKey) => number | undefined;
 
 export const createTriggersCounter = (): TriggersCounter => {
-  const memMap = memoizeMapOnceStateCreated;
+  const memMap = originalMap;
 
   return (key: InterstateKey): number | undefined => {
     const stateEntry = memMap.get(key);
@@ -48,4 +34,4 @@ export const createTriggersCounter = (): TriggersCounter => {
   };
 };
 
-export { mockedCreateState as createState };
+export const { createState }: typeof CreateStateImport = jest.requireActual('../createState.ts');
