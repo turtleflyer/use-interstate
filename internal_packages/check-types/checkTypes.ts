b64437f98eceddc96a32adf533382c7d9ff8aa57
Andrei Kovalev <35089047+turtleflyer@users.noreply.github.com>
2021-03-29 20:34:50 -0700

    Rewrite internal package "check-types"

--- internal_packages/check-types/checkTypes.ts
+++ internal_packages/check-types/checkTypes.ts
@@ -1,8 +1,7 @@
-export type Reveal<T extends never> = T;
+export type IsEqual<T1, T2> = (<G1>() => G1 extends T1 ? 1 : 2) extends <G2>() => G2 extends T2
+  ? 1
+  : 2
+  ? true
+  : false;
 
-export type ToBeExact<T1, T2> = ((a: T1, b: keyof T1) => [T1, keyof T1]) extends (
-  a: T2,
-  b: keyof T2
-) => [T2, keyof T2]
-  ? never
-  : unknown;
+export type IsTrue<T extends true> = T;
